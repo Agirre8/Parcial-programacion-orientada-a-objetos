b9ac813b08792cf7cc5f50ed3c8d5b3a36237f5c
Agirre8 <inagi30@gmail.com>
2023-03-24 11:45:40 +0100

    clase cuenta acabada

--- Ej3.py
+++ Ej3.py
@@ -23,7 +23,19 @@ class Cuenta:
         saldo = saldo + ingresar
         print("Ha ingresado {} euros, su saldo ahora es de {} euros".format((ingresar), (saldo)))
 
+    def transferir(self, saldo, transferir):
+        self.saldo = saldo
+        self.transferir = transferir
+        numeroCuenta2 = input("Ingrese el número de cuenta al que le va ha transferir el dinero: ")
+
+        if saldo > transferir:
+            saldo = saldo - transferir
+            print("Ha transferido {} euros a la siguiente cuenta bancaria: {}, su saldo ahora es de {} euros".format((transferir), (numeroCuenta2), (saldo)))
+        else:
+            print("Saldo insuficiente")
+
+
 cuenta = Cuenta()
 cuenta.retirar(2000, 300)
 cuenta.ingresar(2000, 445)
-
+cuenta.transferir(2000, 300)
