11210a4b0f1ec02291cf9c038f8e98bf00d3b97f
Agirre8 <inagi30@gmail.com>
2023-03-24 11:17:43 +0100

    ejercicio 2 acabado

--- Ej2.py
+++ Ej2.py
@@ -1,17 +1,14 @@
+class Animal:
+    def __innit__(self,):
+        self.edad = 3
+        self.sexo = "hembra"
+    
+    def comer(self):
+        print("El animal esta comiendo")
 
+    def dormir(self):
+        print("El animal esta durmiendo")
 
-
-class Pollo(Animal, Oviparo):
-    def __innit__(self):
-        super().__init__()
-        
-
-class Gato(Animal, Mamifero):
-    def __innit__(self):
-        super().__init__()
-
-    def miau():
-        print("MIAUUUUUUUU MIAUUUUUUUU")
 
 class Oviparo(Animal):
 
@@ -26,21 +23,22 @@ class Mamifero(Animal):
     def __innit__(self, embarazo):
         super().__init__()
         self.embarazo = embarazo
+      
+class Pollo(Oviparo):
+    def __innit__(self):
+        super().__init__()
+        
+
+class Gato(Mamifero):
+    def __innit__(self):
+        super().__init__()
+
+    def miau():
+        print("MIAUUUUUUUU MIAUUUUUUUU")
+
     
     def parto(self):
         print("El animal esta de aprto")
 
-class Animal:
-    def __innit__(self, edad, sexo):
-        self.edad = edad
-        self.sexo = sexo
-    
-    def comer(self):
-        print("El animal esta comiendo")
-
-    def dormir(self):
-        print("El animal esta durmiendo")
-    
-pollo = Animal(3, "femenino")
-
+pollo = Pollo()
 pollo.aovar()
