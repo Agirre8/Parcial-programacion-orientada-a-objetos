0e17decd65b6c56b24c2684a4dd5ba263c0651c2
Agirre8 <inagi30@gmail.com>
2023-03-24 11:06:16 +0100

    clase oviparo

--- Ej2.py
+++ Ej2.py
@@ -1,5 +1,22 @@
 
 
+class Oviparo(Animal):
+    super().__innit__()
+
+    def __innit__(self, huevos):
+        self.huevos = huevos
+    def aovar(self):
+        print("El animal está poniendo huevos")
+
+class Mamifero(Animal):
+
+    super().__innit__()
+    def __innit__(self, embarazo):
+        self.embarazo = embarazo
+    
+    def parto(self):
+        print("El animal esta de aprto")
+
 class Animal:
     def __innit__(self, edad, sexo):
         self.edad = edad
