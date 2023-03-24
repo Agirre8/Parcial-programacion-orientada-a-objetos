9047b1f719ecf077970f0280eb11f31edb4565d8
Agirre8 <inagi30@gmail.com>
2023-03-24 11:00:28 +0100

    clase animal

---
+++ Ej2.py
@@ -0,0 +1,13 @@
+
+
+class Animal:
+    def __innit__(self, edad, sexo):
+        self.edad = edad
+        self.sexo = sexo
+    
+    def comer(self):
+        print("El animal esta comiendo")
+
+    def dormir(self):
+        print("El animal esta durmiendo")
+    
